2f56b4545c774abdba9f4873b0f6d7a14136f8c0
James Kolsby <jrkolsby@mac.com>
2014-02-23 20:19:00 -0500

    Changed half post behavior

--- js/home.js
+++ js/home.js
@@ -1,10 +1,20 @@
+var something = 0,
+	loadPost = function(half) {
+		var content = {
+			"title": half.children("h1").html(),
+			"body": half.children("h2").html()
+		}
+		$("#post .inner h1").html(content['title']);	
+		$("#post .inner h2").html(content['body']);	
+	}
 $(document).ready(function() {
-	$('#blur').click(function() {
+	$('#post').click(function() {
 		$('article.half.active').removeClass('active');
 		$(this).removeClass('active');
 	});
 	$('article.half').click(function() {
 		$(this).addClass('active');
-		$('#blur').addClass('active');
+		loadPost($(this));
+		$('#post').addClass('active');
 	});
 });
